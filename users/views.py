3b5fb389cdcb0eb796a0f3bda5e8e85b524ddb21
s3656673 <s3656673@student.rmit.edu.au>
2019-08-07 13:44:29 +1000

    Refactored signup page

--- users/views.py
+++ users/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
-from .forms import UsersForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+# from .forms import UsersForm
 
 # Create your views here.
 
+# def users_signup_view(request, *args, **kwargs):
+#     form = UsersForm(request.POST or None)
+#     if (form.is_valid()):
+#         form.save()
+#         form = UsersForm()
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'users/users_signup.html', context)
+
 def users_signup_view(request, *args, **kwargs):
-    form = UsersForm(request.POST or None)
-    if (form.is_valid()):
-        form.save()
-        form = UsersForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'users/users_signup.html', context)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('pages:index')
+            # log user in
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/users_signup.html', {'form': form})
