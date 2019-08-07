f7c119b7bd376e903bbbe35cde09bfdfd172ad36
s3656673 <s3656673@student.rmit.edu.au>
2019-08-07 11:11:19 +1000

    Created database model, implemented to admin site.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Users, Trading_Account, Stock_Amount
+
+admin.site.register(Users)
+admin.site.register(Trading_Account)
+admin.site.register(Stock_Amount)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def users_signup_view(request, *args, **kwargs):
+    return render(request, 'users/users_signup.html', {})
