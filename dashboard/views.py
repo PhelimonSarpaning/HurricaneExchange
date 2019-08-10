30731ff223dbb3d59ff6058c87f508d8d4e2026d
Ben Randall <s3603315@student.rmit.edu.au>
2019-08-10 16:18:20 +1000

    updated base to have dashboard button when logged in, user auth on dashboard view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def index(request):
-	return HttpResponse("Hello, world. You're at the dashboard index.")
+@login_required(login_url='/users/')
+def dashboard(request):
+	template = loader.get_template('dashboard/index.html')
+	username = request.user.get_username()
+	context = {
+		'username': username
+	}
+	return render(request,'dashboard/index.html', context)
