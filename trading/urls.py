4dba58bfbf8f5291d714c7e5edfcc7099732d7af
s3656673@student.rmit.edu.au <s3656673@student.rmit.edu.au>
2019-08-28 11:46:37 +1000

    Ability to delete trading account

--- trading/urls.py
+++ trading/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import (
     trading_create_view,
     trading_list_view,
-    trading_detail_view
+    trading_detail_view,
+    trading_delete_view
 )
 
 app_name = 'trading'
 urlpatterns = [
     path('', trading_list_view, name='list'),
     path('create/', trading_create_view, name='create'),
-    path('<int:id>/', trading_detail_view, name='detail')
+    path('<int:id>/', trading_detail_view, name='detail'),
+    path('delete/<int:id>/', trading_delete_view, name='delete')
 ]
