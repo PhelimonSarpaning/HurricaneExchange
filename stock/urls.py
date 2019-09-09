35e68d9dcddda4aaf4ff3f27a2261b7626f25fd4
Caitlin <s3610162@student.rmit.edu.au>
2019-09-09 18:38:06 +1000

    share table

--- stock/urls.py
+++ stock/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('create/<int:id>/', stock_create_view, name='create'),
     path('<int:id>/', stock_detail_view, name='detail'),
     path('stocklist', stock_list_view, name='list'),
-    path('sell/', stock_sell, name='sell'),
+    path('sell/<str:stock_ticker>', stock_sell, name='sell'),
     path('buy/<str:stock_ticker>/', stock_buy, name='buy')
 ]
