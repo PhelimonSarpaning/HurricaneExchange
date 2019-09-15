98ec194259fb668b601d42f1b8c426638de6a8e5
s3656673@student.rmit.edu.au <s3656673@student.rmit.edu.au>
2019-09-15 10:53:55 +1000

    Fixed does not exist error

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -1,8 +1,14 @@
 from trading.models import Trading_Account
 
 def list_trading_accounts(request):
-    queryset = Trading_Account.objects.filter(user_id=request.user.id)
-    defaultTrading = Trading_Account.objects.get(user_id=request.user.id, is_default=True)
+    try:
+        queryset = Trading_Account.objects.filter(user_id=request.user.id)
+    except Trading_Account.DoesNotExist:
+        queryset = None
+    try:
+        defaultTrading = Trading_Account.objects.get(user_id=request.user.id, is_default=True)
+    except Trading_Account.DoesNotExist:
+        defaultTrading = None
     context = {
         'list_accounts': queryset,
         'default_trading': defaultTrading
