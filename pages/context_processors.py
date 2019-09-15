84be247470260b1e35317de32ef891ce5b3aa663
s3656673@student.rmit.edu.au <s3656673@student.rmit.edu.au>
2019-09-15 10:45:29 +1000

    Show default account in dropdown menu

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -2,7 +2,9 @@ from trading.models import Trading_Account
 
 def list_trading_accounts(request):
     queryset = Trading_Account.objects.filter(user_id=request.user.id)
+    defaultTrading = Trading_Account.objects.get(user_id=request.user.id, is_default=True)
     context = {
         'list_accounts': queryset,
+        'default_trading': defaultTrading
     }
     return context
