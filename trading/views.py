573c7dbd0edf350441b4f712883185af929f7105
s3656673@student.rmit.edu.au <s3656673@student.rmit.edu.au>
2019-09-18 09:08:59 +1000

    Removed views debugging prints

--- trading/views.py
+++ trading/views.py
@@ -106,9 +106,6 @@ def trading_history_view(request, *args, **kwargs):
         if form.is_valid():
             startDate = form.cleaned_data['date']
             endDate = form.cleaned_data['date2']
-            print (startDate)
-            print (endDate)
-            
             try: 
                 qs = Transaction_History.objects.filter(date_of_transaction__date__range=[startDate, endDate])
             except Transaction_History.DoesNotExist:
