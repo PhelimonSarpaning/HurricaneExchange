f0653a4dc8c55695ffdef2dadf1931e9752ef08f
Dylan Johnson <hidylanj@gmail.com>
2019-09-17 11:12:04 +1000

    Created model and views for leaderboard

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+class Leaderboard(models.Model):
+    user_id = models.IntegerField(default=100.0)
+    fund = models.BigIntegerField(default=1000.0)
--- leaderboard/views.py
+++ leaderboard/views.py
@@ -3,7 +3,20 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 
+from leaderboard.models import Leaderboard
+from users.models import UserFund
+
 @login_required(login_url="/users")
 def leaderboard_view(request, *args, **kwargs):
-    return render(request, 'leaderboard_view.html', {})
+    fundlist = UserFund.objects
+    if (fundlist.exists()):
+        context = {
+            'object': fundlist,
+            'noFundlist': False
+        }
+    else:
+        context = {
+            'noFundlist': True
+        }
+    return render(request, 'leaderboard_view.html', context)
     
