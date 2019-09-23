251a639c1027a8e6b2eef1d78e8df27dbdfd5d0e
ben <s3603315@student.rmit.edu.au>
2019-09-23 22:01:50 +1000

    extra info when buying stocks

--- stock/views.py
+++ stock/views.py
@@ -209,6 +209,7 @@ def stock_buy(request, stock_ticker, *args, **kwargs):
         'stock_ticker': stock_ticker,
         'stock_name': stock.stock_name,
         'stock_price' : stock.stock_price,
+        'stock_dayChange' : stock.stock_dayChange,
         'stock_available': stock_available,
         'trading_accounts': trading_accounts,
         'form': form,
