f6fafc4ea658780bdd9b86d6b13ad5dd3ffb0c0a
Dylan Johnson <hidylanj@gmail.com>
2019-09-23 17:34:00 +1000

    Fixed up leaderboard issues

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -3,15 +3,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 
-from leaderboard.models import Leaderboard
 from users.models import UserFund
 
 @login_required(login_url="/users")
 def leaderboard_view(request, *args, **kwargs):
-    fundlist = UserFund.objects
-    if (fundlist.exists()):
+    query = UserFund.objects.all()
+    if (query.exists()):
         context = {
-            'object': fundlist,
+            'fundlist': query,
             'noFundlist': False
         }
     else:
