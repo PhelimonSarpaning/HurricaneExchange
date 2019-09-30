8de268419b63d24f183aee42d942139c8e31fba7
ben <s3603315@student.rmit.edu.au>
2019-09-30 16:19:31 +1000

    added more details to admin pages, now lists name and values instead of ID on list view, can view user funds and strading accounts on user page

--- trading/admin.py
+++ trading/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Trading_Account
 
-admin.site.register(Trading_Account)
+class TradingAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'trading_name')
+
+
+admin.site.register(Trading_Account, TradingAdmin)
