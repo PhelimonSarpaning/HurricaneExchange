1024795e893a476a3b1b74f51bc8b2a3709c1f83
ben <s3603315@student.rmit.edu.au>
2019-10-03 13:01:51 +1000

    user asset value is stored each time it is updated as new line in table for historical tracking

--- users/models.py
+++ users/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 
 class UserFund(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
     fund = models.FloatField(default=1000000)
     totalAssetValue = models.FloatField(default=1000000)
+
+class UserAssetValue(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    totalAssetValue = models.FloatField(default=1000000)
+    date_tracked = models.DateTimeField(auto_now_add=True)
